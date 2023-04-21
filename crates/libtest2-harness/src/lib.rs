625629c1b485d6a08472877cf2e08351200e87e6
Ed Page <eopage@gmail.com>
2023-04-21 15:10:44 -0500

    docs(harness): Provide usage example

--- crates/libtest2-harness/src/lib.rs
+++ crates/libtest2-harness/src/lib.rs
@@ -1,4 +1,24 @@
 //! An experimental replacement for the core of libtest
+//!
+//! # Usage
+//!
+//! To use this, you most likely want to add a manual `[[test]]` section to
+//! `Cargo.toml` and set `harness = false`. For example:
+//!
+//! ```toml
+//! [[test]]
+//! name = "mytest"
+//! path = "tests/mytest.rs"
+//! harness = false
+//! ```
+//!
+//! And in `tests/mytest.rs` you would call [`run`] in the `main` function:
+//!
+//! ```no_run
+//! libtest2_harness::Harness::new()
+//!     .main();
+//! ```
+//!
 
 pub mod cli {
     pub use lexarg::*;
